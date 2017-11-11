c12f56a39938c02cbbe96f109a3b9ad73b08f12e
Daniel <dd.castaneda10@uniandes.edu.co>
2017-11-11 18:39:13 -0500

    Recorridos

--- Ondas.c
+++ Ondas.c
@@ -31,22 +31,31 @@ fclose(data);
 
 // Resolver ecuacion de onda:
 float *Inicio = malloc(len*sizeof(float));
-float *Modife = malloc(len*sizeof(float));
+float *Modify = malloc(len*sizeof(float));
 float *Time = malloc(len*sizeof(float));
-float alpha = 10.0
-float omega = 10.0
-float cambio = 1.0/dt
-float r = (pow(c,2)*pow(dt, 2))/pow(dx, 2)
-int i, j;
-for(i=0;i<cambio;i++) //Recorrido para tiempos
+float alpha = 10.0;
+float omega = 10.0;
+float cambio = 1.0/dt;
+float r = (pow(c,2)*pow(dt, 2))/pow(dx, 2);
+float Form1=0.0;
+float Form2=0.0;
+float Form3=0.0;
+int j;
+for(i=0;i<5;i++) //Recorrido para tiempos
 {
-	for(j=1;j<len;j++) //Recorrido para nodos
+	for(j=1;j<5;j++) //Recorrido para nodos
 {
 		if(i=0)
-{
-			Modife[j] = (Inicio[j-1]-2*Inicio[j]+Inicio[j+1])*r+(2*Inicio-0);
+{			
+			Form1 = (Inicio[j-1]-2*Inicio[j]+Inicio[j+1])*r;
+			Form2 = (2.0*Inicio[j]);
+			Form3 = (2.0*Inicio[j]-Time[j]);
+			Modify[j] = Form1+Form2;
 }		
-		
+		else
+{
+			Modify[j] = Form1+Form3;
+}
 }
 
 }
